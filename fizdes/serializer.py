8c825ae2d28afa3fc254da45556a92ebacdad79e
Anvar Sobirov <asobirov9083@gmail.com>
2022-12-24 12:31:32 +0500

    more changes

--- fizdes/serializer.py
+++ fizdes/serializer.py
@@ -1,6 +1,8 @@
+import io
+from rest_framework.parsers import JSONParser
 from rest_framework import  serializers
 from rest_framework.renderers import JSONRenderer
-
+from .models import ToDo
 # ToDo Model obyektlarni olish uchun
 class ToDoModel:
     def __init__(self, title, description, completed=False):
@@ -13,12 +15,22 @@ class ToDoSerializer(serializers.Serializer):
     title = serializers.CharField()
     description = serializers.CharField()
     completed = serializers.BooleanField(default=False)
+    created = serializers.DateTimeField(read_only=True)
+    updated = serializers.DateTimeField(read_only=True)
+
+    def create(self, validated_data):
+        return ToDo.objects.create(**validated_data)
 
 # encode func
 def encode():
     model = ToDoModel(title="Kechqurun kitob o'qish", description="Har kuni kechqurun kitob o'qish")
     model_sr = ToDoSerializer(instance=model)
-    print(model_sr.data, type(model_sr.data), sep="  va  ")
     json = JSONRenderer().render(model_sr.data)
     print(json)
 
+def decode():
+    stream = io.BytesIO(b'{"title":"Kechqurun kitob o\'qish","description":"Har kuni kechqurun kitob o\'qish","completed":false}')
+    data = JSONParser().parse(stream)
+    serializer = ToDoSerializer(data=data)
+    serializer.is_valid()
+    print(serializer.validated_data)
--- fizdes/urls.py
+++ fizdes/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import todo_list, get_todo_detail
+from .views import todo_list#, get_todo_detail
 
 
 urlpatterns = [
     path('', todo_list, name="todo_list"),
-    path('<int:todo_id>/', get_todo_detail, name="get_todo_detail")
+    # path('<int:todo_id>/', get_todo_detail, name="get_todo_detail")
 ]
 
